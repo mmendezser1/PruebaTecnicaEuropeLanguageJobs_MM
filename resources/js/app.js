512a1d2e6d2756d089ea9f5d171eedb46fb95151
marcos <marcosmendezser1@gnail.com>
2023-11-23 19:29:04 +0100

    remove comments on App.JS and file bootstrap.js

--- resources/js/app.js
+++ resources/js/app.js
@@ -1,13 +1,9 @@
-// app.js
 import "bootstrap/dist/css/bootstrap.css";
 import { createApp } from "vue";
 import App from "./App.vue";
-import routes from "./routes"; // Importa tus rutas desde routes.js
-
+import routes from "./routes";
 const app = createApp(App);
 
-// Configura las rutas
 app.use(routes);
 
-// Monta la aplicación en el elemento con el id "app"
 app.mount("#app");
