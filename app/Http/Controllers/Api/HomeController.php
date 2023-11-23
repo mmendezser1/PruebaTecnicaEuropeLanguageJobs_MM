85a451cd583893247477a2513afb405d08ae2ff2
marcos <marcosmendezser1@gnail.com>
2023-11-23 20:51:33 +0100

    CreateDog method Back

--- app/Http/Controllers/Api/HomeController.php
+++ app/Http/Controllers/Api/HomeController.php
@@ -5,16 +5,60 @@ namespace App\Http\Controllers\Api;
 use App\Http\Controllers\Controller;
 use App\Models\Dog;
 use Illuminate\Http\Request;
+use Illuminate\Support\Facades\Validator;
 
 class HomeController extends Controller
 {
     public function getDogs(Request $req)
     {
         $dogs = Dog::all();
+
         return response()->json([
             'dogs' => $dogs,
-
         ], 200);
+
+    }
+
+    public function createDog(Request $req)
+    {
+
+        $fileName = $req->file('image')->getClientOriginalName();
+
+        try {
+
+            $responseValidation = Validator::make($req->all(), [
+                'image' => 'required|file',
+                'name' => 'required',
+                'race' => 'required',
+                'age_months' => 'required|integer',
+                'size' => 'required',
+                'hair_color' => 'required',
+                'hair_style' => 'required',
+            ]);
+
+            if ($responseValidation->fails()) {
+                return response()->json(['message' => "Error on server: " . $responseValidation->messages()], 500);
+            }
+
+            $req->file('image')->move(public_path('images'), $fileName);
+            //No puedo guardar el new Dog así => Dog::create($responseValidation->validated());
+            //Porque en el image solo quiero guardar el nombre del archivo y le paso un file entero.
+
+            Dog::create([
+                'image' => $fileName,
+                'name' => $req->input('name'),
+                'race' => $req->input('race'),
+                'age_months' => $req->input('age_months'),
+                'size' => $req->input('size'),
+                'hair_color' => $req->input('hair_color'),
+                'hair_style' => $req->input('hair_style')]);
+
+            return response()->json(['message' => "Dog created correctly"], 200);
+
+        } catch (\Throwable $th) {
+            return response()->json(['message' => "Error on server: " . $th->getMessage()], 500);
+
+        }
 
     }
 }
--- routes/api.php
+++ routes/api.php
@@ -19,3 +19,4 @@ use Illuminate\Support\Facades\Route;
 // });
 
 Route::get('/dogs', [HomeController::class, 'getDogs']);
+Route::post('/create/dog', [HomeController::class, 'createDog']);
