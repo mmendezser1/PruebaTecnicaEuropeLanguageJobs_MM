7c26a5dc9cf0440cb62e75dd034a3bb0af7de089
marcos <marcosmendezser1@gnail.com>
2023-11-24 12:51:43 +0100

    Home View

--- resources/js/Views/Home.vue
+++ resources/js/Views/Home.vue
@@ -1,9 +1,39 @@
 <template>
-    <h1>Hello World from Home.vue</h1>
-    <button @click="addDog">CREATE DOG</button>
     <article class="welcome">
-        <!-- <img> -->
+        <div class="row banner-container">
+            <div class="col-md-5 col-xs-12">
+                <h1 class="title welcome__tittle">
+                    Europe languages jobs, also likes dogs!
+                </h1>
+                <button href="/companies/registry" class="welcome__btn">
+                    ADD DOG
+                </button>
+            </div>
+            <div class="col-md-5 offset-md-2 col-xs-12">
+                <div class="item-list">
+                    <span
+                        ><strong class="item-list__strong">+ 2,500</strong
+                        ><br />Dogs races
+                    </span>
+                </div>
+                <div class="item-list">
+                    <span
+                        ><strong class="item-list__strong"
+                            >Post with ease</strong
+                        ><br />Publish a dog info in just 2 minutes
+                    </span>
+                </div>
+                <div class="item-list">
+                    <span
+                        ><strong class="item-list__strong">+ 50,000</strong
+                        ><br />Dogs added to the platform
+                    </span>
+                </div>
+            </div>
+        </div>
     </article>
+
+    <button @click="addDog">CREATE DOG</button>
     <article class="dogs_items">
         <div v-for="item in dogs" :key="item.id">
             <Dog :dog="item" />
@@ -15,6 +45,7 @@
 import api from "../api/index";
 import Dog from "../Components/Dog.vue";
 import DogModel from "../Models/Dog";
+import "../assets/home.css";
 export default {
     name: "Home",
     components: { Dog },
