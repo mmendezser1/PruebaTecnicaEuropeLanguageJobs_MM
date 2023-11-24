ae06896c5fd06c666c1c05cfe719beded659fff2
marcos <marcosmendezser1@gnail.com>
2023-11-24 12:52:53 +0100

    navbar class & style

--- resources/js/Components/NavBar.vue
+++ resources/js/Components/NavBar.vue
@@ -1,8 +1,12 @@
 <template>
-    <nav class="navbar">THIS IS MY NAVBAR</nav>
+    <nav class="custom_navbar">
+        <img src="images/logo.svg" class="custom_navbar__img" />
+    </nav>
 </template>
 
 <script>
+import "../assets/navbar.css";
+
 export default {
     name: "NavBar",
 };
