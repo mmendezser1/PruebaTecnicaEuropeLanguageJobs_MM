2f3d4fda5832f4bdbddcd265a188297549d19f6b
marcos <marcosmendezser1@gnail.com>
2023-11-24 19:02:42 +0100

    Add library SweetAlert2

--- resources/js/app.js
+++ resources/js/app.js
@@ -1,8 +1,12 @@
 import "bootstrap/dist/css/bootstrap.css";
+import Swal from "sweetalert2";
+
 import { createApp } from "vue";
 import App from "./App.vue";
 import routes from "./routes";
 const app = createApp(App);
+
+app.config.globalProperties.$swal = Swal; // Esto permite usarlo fácilmente en tus componentes Vue
 
 app.use(routes);
 
